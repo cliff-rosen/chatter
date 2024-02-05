1f1cd4630cda3b52fe0d52a31f1a276cf64f3f3c
Cliff Rosen <cliff.rosen@gmail.com>
2024-02-05 07:34:10 -0500

    b

--- backend/chatter/api/answer.py
+++ backend/chatter/api/answer.py
@@ -30,21 +30,15 @@ set COMPLETION_MODEL_TIKTOKEN for gpt-4
 
 logger = logging.getLogger()
 
-COMPLETION_MODEL_TIKTOKEN = 'text-davinci-003'
+COMPLETION_MODEL_TIKTOKEN = "text-davinci-003"
 MAX_TOKEN_COUNT = 8000
 MAX_CHUNKS_TOKEN_COUNT = 2500
 MAX_RESPONSE_TOKENS = 400
 
 
 def get_answer(
-                conversation_id,
-                domain_id,
-                query, 
-                initial_prompt,
-                temperature,
-                user_id,
-                deep_search
-            ):
+    conversation_id, domain_id, query, initial_prompt, temperature, user_id, deep_search
+):
     """
     Generate an answer from the LLM based on the following items:
 
@@ -53,43 +47,45 @@ def get_answer(
         initial assistant message: retrieved from db based on domain
         conversation history: retrieved from conversation_id
         user query: supplied as query
-    """    
+    """
 
-    #temporarily force temp to 0
+    # temporarily force temp to 0
     temperature = 0.0
 
-    print('get_answer -------------------------------->')
+    print("get_answer -------------------------------->")
     use_context = False
-    prompt_context = ''
+    prompt_context = ""
     context_chunks = {}
     initial_message = conf.DEFAULT_INITIAL_MESSAGE
     conversation_history = []
 
     print("getting domain settings")
     res = db.get_domain(domain_id)
-    if res['initial_conversation_message']:
-        initial_message = res['initial_conversation_message']
-    if res['use_context']:
+    if res["initial_conversation_message"]:
+        initial_message = res["initial_conversation_message"]
+    if res["use_context"]:
         use_context = True
 
     print("getting conversation history")
-    if conversation_id != 'NEW':
+    if conversation_id != "NEW":
         conversation_history = db.get_conversation_history(conversation_id)
-        print('  conversation_id: ', conversation_id, 'length', len(conversation_history))
+        print(
+            "  conversation_id: ", conversation_id, "length", len(conversation_history)
+        )
 
     print("getting context chunks")
     if use_context:
         context_chunks = kb.get_chunks_from_query(domain_id, query)
 
-    print('creating context from chunks')
+    print("creating context from chunks")
     prompt_context = create_prompt_context(
-            initial_prompt,
-            initial_message,
-            conversation_history,
-            query,
-            context_chunks,
-            MAX_RESPONSE_TOKENS
-        )
+        initial_prompt,
+        initial_message,
+        conversation_history,
+        query,
+        context_chunks,
+        MAX_RESPONSE_TOKENS,
+    )
 
     print("creating prompt messages")
     messages = create_prompt_messages(
@@ -99,40 +95,40 @@ def get_answer(
         conversation_history,
         query,
     )
-    logger.info('Prompt:\n' + str(query))
+    logger.info("Prompt:\n" + str(query))
     if not messages:
         return {"status": "BAD_REQUEST"}
-    
+
     print("querying model")
     response = query_model(messages, temperature)
 
     print("updating conversation tables")
     conversation_id = update_conversation_tables(
-                            domain_id,
-                            query, 
-                            initial_prompt,
-                            prompt_context,
-                            initial_message,
-                            temperature,
-                            conversation_id,
-                            conversation_history,
-                            response,
-                            context_chunks,
-                            user_id
-                        )
+        domain_id,
+        query,
+        initial_prompt,
+        prompt_context,
+        initial_message,
+        temperature,
+        conversation_id,
+        conversation_history,
+        response,
+        context_chunks,
+        user_id,
+    )
 
-    print('get_answer completed')
+    print("get_answer completed")
     return {
-            "conversation_id": conversation_id,
-            "answer": response,
-            "chunks": context_chunks,
-            "chunks_used_count": len(list(context_chunks.keys())) 
-        }
+        "conversation_id": conversation_id,
+        "answer": response,
+        "chunks": context_chunks,
+        "chunks_used_count": len(list(context_chunks.keys())),
+    }
 
 
 def num_tokens(*args):
     token_count = 0
-    text = ''
+    text = ""
     for arg in args:
         text += arg
     token_count += num_tokens_from_string(text, COMPLETION_MODEL_TIKTOKEN)
@@ -141,123 +137,146 @@ def num_tokens(*args):
 
 def create_conversation_history_text(conversation_history):
     conversation_text = ""
-    user = 'User'
-    ai = 'Assistant'
-    user_key = 'query_text'
-    assistant_key = 'response_text'
+    user = "User"
+    ai = "Assistant"
+    user_key = "query_text"
+    assistant_key = "response_text"
 
     try:
         for row in conversation_history:
-            conversation_text += f"{user}: {row[user_key]}\n{ai}: {row[assistant_key]}\n\n"
+            conversation_text += (
+                f"{user}: {row[user_key]}\n{ai}: {row[assistant_key]}\n\n"
+            )
     except Exception as e:
-        raise InputError('Bad conversationHistory record:' + str(conversation_history))
+        raise InputError("Bad conversationHistory record:" + str(conversation_history))
     return conversation_text
 
 
 def create_prompt_context(
-        initial_prompt,
-        initial_message,
-        conversation_history,
-        user_message,
-        chunks,
-        max_tokens
-        ):
-    context_for_prompt = ''
+    initial_prompt,
+    initial_message,
+    conversation_history,
+    user_message,
+    chunks,
+    max_tokens,
+):
+    context_for_prompt = ""
     max_token_count = MAX_TOKEN_COUNT
 
     if not chunks:
-        return ''
+        return ""
 
     conversation_history_text = create_conversation_history_text(conversation_history)
-    prompt_token_count = num_tokens(initial_prompt, conversation_history_text, initial_message, user_message)
-    print('tokens used by pre-context prompt: %s' % (prompt_token_count))
-    max_context_token_count = max_token_count - prompt_token_count - max_tokens
+    prompt_token_count = num_tokens(
+        initial_prompt, conversation_history_text, initial_message, user_message
+    )
+    print("tokens used by pre-context prompt: %s" % (prompt_token_count))
+    max_context_token_count = max_token_count - prompt_token_count - max_tokens - 400
     context_for_prompt = process_chunks_and_get_as_text(chunks, max_context_token_count)
 
     return context_for_prompt
 
 
-def process_chunks_and_get_as_text(chunks, max_chunks_token_count = MAX_CHUNKS_TOKEN_COUNT):
-    print('process_chunks_and_get_as_text using max token count of', max_chunks_token_count)
+def process_chunks_and_get_as_text(
+    chunks, max_chunks_token_count=MAX_CHUNKS_TOKEN_COUNT
+):
+    print(
+        "process_chunks_and_get_as_text using max token count of",
+        max_chunks_token_count,
+    )
     context = ""
     chunks_token_count = 0
     chunks_used_count = 0
 
-    for id, chunk in sorted(chunks.items(), key=lambda item: item[1]["score"], reverse=True):
-        tokens_in_chunk = num_tokens_from_string(chunk['text'], COMPLETION_MODEL_TIKTOKEN)
+    for id, chunk in sorted(
+        chunks.items(), key=lambda item: item[1]["score"], reverse=True
+    ):
+        tokens_in_chunk = num_tokens_from_string(
+            chunk["text"], COMPLETION_MODEL_TIKTOKEN
+        )
         if chunks_token_count + tokens_in_chunk > max_chunks_token_count:
-            print(' chunk', id, 'too long to fit.  moving on.')
-            chunks[id]['used'] = False
+            print(" chunk", id, "too long to fit.  moving on.")
+            chunks[id]["used"] = False
             continue
-        context = context + chunk['text'] + '\n\n'
-        chunks[id]['used'] = True
+        chunk_meta_data = f"chunk_id: {str(id)}, filename: {chunk['uri']}"
+        context = (
+            context
+            + "<CHUNK>"
+            + chunk_meta_data
+            + "\n"
+            + chunk["text"]
+            + "</CHUNK>\n\n"
+        )
+        chunks[id]["used"] = True
         chunks_used_count += 1
         chunks_token_count += tokens_in_chunk
-    print('chunks provided: %s, chunks used: %s, tokens used: %s' % (len(chunks), chunks_used_count, int(chunks_token_count)))
+    print(
+        "chunks provided: %s, chunks used: %s, tokens used: %s"
+        % (len(chunks), chunks_used_count, int(chunks_token_count))
+    )
 
     if context:
-        return '<START OF CONTEXT>\n' + context.strip() + '\n<END OF CONTEXT>'
+        return "<START OF CONTEXT>\n" + context.strip() + "\n<END OF CONTEXT>"
     else:
-        return ''
+        return ""
 
 
 def create_prompt_text(
-                        initial_prompt,
-                        prompt_context,
-                        initial_message,
-                        conversation_history,
-                        query
-                    ):
-    user_role = 'User: '
-    bot_role = 'Assistant: '
+    initial_prompt, prompt_context, initial_message, conversation_history, query
+):
+    user_role = "User: "
+    bot_role = "Assistant: "
 
     conversation_history_text = ""
     prompt = ""
 
     system_message = initial_prompt
     if prompt_context:
-        system_message = system_message + '\n\n' + prompt_context    
+        system_message = system_message + "\n\n" + prompt_context
 
     conversation_history_text = create_conversation_history_text(conversation_history)
 
-    prompt = system_message + '\n\n' \
-        + bot_role + initial_message + '\n\n' \
-        + conversation_history_text  \
-        + user_role + query + '\n' \
+    prompt = (
+        system_message
+        + "\n\n"
         + bot_role
+        + initial_message
+        + "\n\n"
+        + conversation_history_text
+        + user_role
+        + query
+        + "\n"
+        + bot_role
+    )
 
     return prompt
 
 
 def create_prompt_messages(
-                            initial_prompt,
-                            prompt_context,
-                            initial_message,
-                            conversation_history,
-                            query
-                        ):
+    initial_prompt, prompt_context, initial_message, conversation_history, query
+):
     messages = []
 
     # add system message
     system_message = initial_prompt
     if prompt_context:
-        system_message = system_message + '\n\n' + prompt_context
+        system_message = system_message + "\n\n" + prompt_context
     messages.append({"role": "system", "content": system_message})
 
     # add initial assistant message
     messages.append({"role": "assistant", "content": initial_message})
 
     # add user and assistant messages from history
-    user_key = 'query_text'
-    assistant_key = 'response_text'
+    user_key = "query_text"
+    assistant_key = "response_text"
     for row in conversation_history:
-        messages.append({"role": "user", "content": row[user_key]})        
-        messages.append({"role": "assistant", "content": row[assistant_key]})   
+        messages.append({"role": "user", "content": row[user_key]})
+        messages.append({"role": "assistant", "content": row[assistant_key]})
 
     # add new user message
     messages.append({"role": "user", "content": query})
 
-    #logger.info(messages)
+    # logger.info(messages)
     return messages
 
 
@@ -266,52 +285,42 @@ def query_model(messages, temperature):
 
 
 def update_conversation_tables(
-                        domain_id,
-                        query,
-                        initial_prompt,
-                        prompt_context,
-                        initial_message,
-                        query_temp,
-                        conversation_id,
-                        conversation_history,
-                        response_text, 
-                        chunks,
-                        user_id
-                    ):
-    
+    domain_id,
+    query,
+    initial_prompt,
+    prompt_context,
+    initial_message,
+    query_temp,
+    conversation_id,
+    conversation_history,
+    response_text,
+    chunks,
+    user_id,
+):
     prompt_text = create_prompt_text(
-                                    initial_prompt,
-                                    prompt_context,
-                                    initial_message,
-                                    conversation_history,
-                                    query
-                                )
+        initial_prompt, prompt_context, initial_message, conversation_history, query
+    )
 
     conversation_text = prompt_text + response_text
 
-    if conversation_id == 'NEW':
+    if conversation_id == "NEW":
         conversation_id = make_new_conversation_id()
-        db.insert_conversation(
-                                conversation_id,
-                                user_id, 
-                                domain_id,
-                                conversation_text
-                            )
+        db.insert_conversation(conversation_id, user_id, domain_id, conversation_text)
     else:
         db.update_conversation(conversation_id, conversation_text)
 
-    response_chunk_ids = ', '.join(list(chunks.keys()))
+    response_chunk_ids = ", ".join(list(chunks.keys()))
 
     db.insert_query_log(
-                        domain_id,
-                        query,
-                        prompt_text,
-                        query_temp,
-                        response_text,
-                        response_chunk_ids,
-                        user_id,
-                        conversation_id
-                    )
-    #logger.info('Conversation:\n' + conversation_text)
+        domain_id,
+        query,
+        prompt_text,
+        query_temp,
+        response_text,
+        response_chunk_ids,
+        user_id,
+        conversation_id,
+    )
+    # logger.info('Conversation:\n' + conversation_text)
 
     return conversation_id
--- backend/chatter/run3.py
+++ backend/chatter/run3.py
@@ -1,4 +1,4 @@
-print('starting')
+print("starting")
 
 import local_secrets as secrets
 import os
@@ -7,11 +7,14 @@ from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 
 OPENAI_API_KEY = secrets.OPENAI_API_KEY
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
+print("loading director...")
+documents = SimpleDirectoryReader("data_1").load_data()
+print("directory loaded.")
 
-documents = SimpleDirectoryReader("data").load_data()
-
-#for document in documents:
-document = documents[0]
-print(document.metadata['file_name'])
+# for document in documents:
+for document in documents:
+    print("===========================================================================")
+    print(document.metadata["file_name"])
+    print(document.text)
